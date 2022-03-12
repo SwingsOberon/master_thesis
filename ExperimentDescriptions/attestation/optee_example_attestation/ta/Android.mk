1862d8762c5eac2f24c88af22d0bfeb25dc85a53
Oberon Swings <Pr3cIze@gmail.com>
2022-03-12 11:15:49 +0100

    wrote an optee example based on hotp which attests some memory

---
+++ ExperimentDescriptions/attestation/optee_example_attestation/ta/Android.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH := $(call my-dir)
+
+local_module := 474d4123-2d81-4825-6820-4a6f420b6542.ta
+include $(BUILD_OPTEE_MK)
---
+++ ExperimentDescriptions/attestation/optee_example_attestation/ta/sub.mk
@@ -0,0 +1,2 @@
+global-incdirs-y += include
+srcs-y += attest_ta.c
